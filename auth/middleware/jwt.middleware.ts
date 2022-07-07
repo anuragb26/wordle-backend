73c36f8ebd35d57d58e098f6ea9aec0c9fe8a4c5
Anurag Bajaj <anuragb26@gmail.com>
2022-07-06 22:55:38 -0400

    Chore:auth route

---
+++ auth/middleware/jwt.middleware.ts
@@ -0,0 +1,74 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import usersService from "../../users/services/users.service";
+
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT not configured");
+}
+const jwtSecret = process.env.JWT_SECRET;
+
+class JwtMiddleware {
+  verifyRefreshBodyField(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (req.body && req.body.refreshToken) {
+      return next();
+    } else {
+      return res
+        .status(400)
+        .send({ errors: ["Missing required field:refreshToken"] });
+    }
+  }
+  async validRefreshNeeded(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const user = await usersService.getUserByEmailWithPassword(
+      res.locals.jwt.email
+    );
+    const salt = crypto.createSecretKey(
+      Buffer.from(res.locals.jwt.refreshKey.data)
+    );
+    const hash = crypto
+      .createHmac("sha512", salt)
+      .update(res.locals.jwt.userId + jwtSecret)
+      .digest("base64");
+    if (hash === req.body.refreshToken && user) {
+      req.body = {
+        userId: user._id,
+        email: user.email,
+        permissionFlags: user.permissionFlags,
+      };
+      return next();
+    } else {
+      return res.status(400).send({ errors: ["Invalid refresh token"] });
+    }
+  }
+  validJwtNeeded(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (req.headers["authorization"]) {
+      try {
+        const authorization = req.headers["authorization"].split(" ");
+        if (authorization[0] !== "Bearer") {
+          return res.status(401).send();
+        } else {
+          res.locals.jwt = jwt.verify(authorization[1], jwtSecret);
+          next();
+        }
+      } catch (err) {
+        return res.status(403).send();
+      }
+    } else {
+      return res.status(401).send();
+    }
+  }
+}
+
+export default new JwtMiddleware();
