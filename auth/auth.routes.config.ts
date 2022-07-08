2363785f911a5bdc8b2fac46eace25e53c5bbf89
Anurag Bajaj <anuragb26@gmail.com>
2022-07-07 22:42:05 -0400

    chore:auth refresh route

--- auth/auth.routes.config.ts
+++ auth/auth.routes.config.ts
@@ -4,6 +4,7 @@ import authMiddleware from "./middleware/auth.middleware";
 import express from "express";
 import bodyValidationMiddleware from "../common/middleware/body.validation.middleware";
 import { body } from "express-validator";
+import jwtMiddleware from "./middleware/jwt.middleware";
 
 export class AuthRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
@@ -15,6 +16,12 @@ export class AuthRoutes extends CommonRoutesConfig {
       body("password").isString(),
       bodyValidationMiddleware.verifyBodyFieldErrors,
       authMiddleware.verifyUserPassword,
+      authController.createJwt,
+    ]);
+    this.app.post("/auth/refresh-token", [
+      jwtMiddleware.validJwtNeeded,
+      jwtMiddleware.verifyRefreshBodyField,
+      jwtMiddleware.validRefreshNeeded,
       authController.createJwt,
     ]);
     return this.app;
